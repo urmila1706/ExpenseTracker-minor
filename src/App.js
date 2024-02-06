e37837d151bb9420c6d665df0c0433e63a2f552c
urmila1706 <raut.urmila88@gmail.com>
2024-02-06 16:39:01 +0530

    Basics of Expense Tracker

--- src/App.js
+++ src/App.js
@@ -3,22 +3,11 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <div>Food Rs 10</div>
+    <div>Petrol Rs 100</div>
+    <div>Movies Rs 200</div>
+    </>
   );
 }
 
