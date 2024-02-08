edababba825f3dfd0ef05606246aff7e1761d413
urmila1706 <raut.urmila88@gmail.com>
2024-02-08 21:42:29 +0530

    Using Props and add LOE

--- src/App.js
+++ src/App.js
@@ -1,14 +1,59 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import ExpenseItem from "./components/ExpenseItem";
 
 function App() {
+  const expenses = [
+    {
+      id: "e1",
+      title: "Toilet Paper",
+      amount: 94.12,
+      date: new Date(2020, 7, 14),
+      LocationOfExpenditure: "Austria",
+    },
+    {
+      id: "e2",
+      title: "New TV",
+      amount: 799.49,
+      date: new Date(2021, 2, 12),
+      LocationOfExpenditure: "Australia",
+    },
+    {
+      id: "e3",
+      title: "Car Insurance",
+      amount: 294.67,
+      date: new Date(2021, 2, 28),
+      LocationOfExpenditure: "Denmark",
+    },
+    {
+      id: "e4",
+      title: "New Mobile",
+      amount: 10000,
+      date: new Date(2021, 6, 12),
+      LocationOfExpenditure: "New Orliance ",
+    },
+    {
+      id: "e5",
+      title: "New Desk (Wooden)",
+      amount: 450,
+      date: new Date(2022, 5, 12),
+      LocationOfExpenditure: "Pyarish ",
+    },
+  ];
+
   return (
-    <>
-    <div>Food Rs 10</div>
-    <div>Petrol Rs 100</div>
-    <div>Movies Rs 200</div>
-    </>
+    <div className="App">
+      <h1>React Js</h1>
+      {expenses.map((expense, key) => {
+        return (
+          <ExpenseItem
+            title={expense.title}
+            amount={expense.amount}
+            date={expense.date}
+            location={expense.LocationOfExpenditure}
+          ></ExpenseItem>
+        );
+      })}
+    </div>
   );
 }
-
 export default App;
