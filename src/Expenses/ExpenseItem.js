f078cc448f72fce3269461974b5c3da5e28786c0
urmila1706 <raut.urmila88@gmail.com>
2024-02-17 20:52:31 +0530

    Adding a filter

--- src/Expenses/ExpenseItem.js
+++ src/Expenses/ExpenseItem.js
@@ -1,28 +1,18 @@
-import React, { useState } from "react";
+import React  from "react";
 import Card from "../UI/Card";
 import ExpenseDate from "../Expenses/ExpenseDate";
 import "./ExpenseItem.css";
-import ExpenseDetails from "../Expenses/ExpenseDetails";
-function ExpenseItem(props) {
-  const [title, setTitle]=useState(props.title);
-  console.log('Expense item should be evaluated by react');
-  const clickHandler = () => {
-   // event.target.parentNode.remove();
-   // title='updated!';
-    // event.target.parentNode.remove();
-    setTitle('updated!');
-    console.log(title);
-  };
+//import ExpenseDetails from "../Expenses/ExpenseDetails";
+const ExpenseItem=(props)=> {
   return (
     <Card className="expense-item">
       <ExpenseDate date={props.date} />
-      <h2>title</h2>
-      <ExpenseDetails
-        title={props.title}
-        location={props.location}
-        amount={props.amount}
-      />
-            <button onClick={clickHandler}>Change Title</button>
+     
+      <div className='expense-item__description'>
+      <h2>{props.title}</h2>
+      <div className='expense-item__price'>${props.amount}</div>
+      </div>
+            
     </Card>
   );
 }
