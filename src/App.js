312eb5f24bd3ac61bf959cb79aa5ea837c9f9026
urmila1706 <raut.urmila88@gmail.com>
2024-02-18 17:50:41 +0530

    Adding New Expenses

--- src/App.js
+++ src/App.js
@@ -1,42 +1,12 @@
+//import React, {useState} from 'react';
 import "./App.css";
 import Expenses from "./Expenses/Expenses";
 import NewExpense from "./Expenses/NewExpense/NewExpense";
-
 function App() {
-  const expenses = [
-    {
-      id: "e1",
-      title: "Toilet Paper",
-      amount: 94.12,
-      date: new Date(2020, 7, 14),
-      LocationOfExpenditure: "Austria",
-    },
-
-    {
-      id: "e2",
-      title: "New TV",
-      amount: 799.49,
-      date: new Date(2021, 2, 12),
-      LocationOfExpenditure: "Australia",
-    },
-    {
-      id: "e3",
-      title: "Car Insurance",
-      amount: 294.67,
-      date: new Date(2021, 2, 28),
-      LocationOfExpenditure: "Denmark",
-    },
-  ];
-  const addExpenseHandler=expense=>{
-    console.log('In App.js');
-    console.log(expense);
-
-  }
   return (
-
-    <div >
-      <NewExpense onAddExpense={addExpenseHandler}/>
-      <Expenses items={expenses}/>
+    <div className="App">
+      <NewExpense />
+      <Expenses></Expenses>
     </div>
   );
 }
